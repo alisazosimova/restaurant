49a39f21a096b7ff16ed9e0ea32873755b8c2bbc
Vadym Khodak <vaddimart@gmail.com>
2019-05-25 13:15:15 +0300

    Update settings.py
    add allowed host

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '5f2194c6.ngrok.io', '127.0.0.1', ]
+ALLOWED_HOSTS = ['localhost', '0.0.0.0', 'replicator.pythonanywhere.com', '127.0.0.1', ]
 
 
 # Application definition
